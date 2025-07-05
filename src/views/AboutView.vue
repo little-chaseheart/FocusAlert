635399342dfb179e1ca7629640a8e912280b9cac
chad <chenzhao200110@foxmail.com>
2025-07-05 16:07:19 +0800

    新建项目，删除不需要部分

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>关于我们</h1>
+    <p>这是一个专注提醒应用。</p>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<script setup lang="ts">
+// 组件逻辑可以在这里添加
+</script>
+
+<style scoped>
+.about {
+  padding: 20px;
+  text-align: center;
+}
+
+h1 {
+  color: #2c3e50;
+  margin-bottom: 20px;
+}
+
+p {
+  color: #666;
+  font-size: 16px;
 }
 </style>
